140c8b688006a0ae77974ad3d8e7c48346d6c77e
triinutamm <triinu.tamm@tptlive.ee>
2019-02-17 00:35:29 +0200

    algus

---
+++ TipCalc/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+
+namespace TipCalc
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+            var SBPercent = FindViewById<SeekBar>(Resource.Id.sbPercent);
+            var TextTip = FindViewById<TextView>(Resource.Id.textTip);
+            var TextSum = FindViewById<TextView>(Resource.Id.textSumInt);
+            var TextBill = FindViewById<EditText>(Resource.Id.textBillInt);
+            double bill = 0;
+            double percent = 0;
+            
+
+            TextBill.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) => {
+
+                bill = Convert.ToInt32(TextBill.Text);
+                TextSum.Text = (bill / 100*percent + bill).ToString() + "€";
+            };
+
+            SBPercent.ProgressChanged += (s, e) => {
+                TextTip.Text = string.Format("Jootraha soovin jätta {0} %", e.Progress);
+                percent = e.Progress;
+                TextSum.Text = (bill / 100 * percent + bill).ToString() + "€";
+            };
+        }
+    }
+}
